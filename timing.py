521dd1ee9417a69de527cfe726c24347ba3bf6f9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-08-16 12:46:56 -0400

    fixing timing script to be more portable

--- timing.py
+++ timing.py
@@ -3,10 +3,15 @@ from urlparse import urljoin
 import sys
 import time
 
-if __name__ == '__main__':
-  # seed website
-  base_url = sys.argv[1]
+def fetch_result(base_url, route):
+  t1 = time.time()
+  response = requests.get(urljoin(base_url, route)).json()
+  t2 = time.time()
+  response['request_time'] = (t2 - t1) / 1000.
+  response['route'] = route
+  return response
 
+def fetch_results(base_url):
   print "Fetching root"
   assert requests.get(urljoin(base_url, '/')).status_code == 200
 
@@ -23,12 +28,19 @@ if __name__ == '__main__':
   requests.post(urljoin(base_url, "/flush_memcache"))
 
   print "Querying ..."
-  for route in queries:
-    t1 = time.time()
-    response = requests.get(urljoin(base_url, route)).json()
-    server_time = response['time']
-    server_clock = response['clock']
-    length = response['length']
-    t2 = time.time()
-    request_time = (t2 - t1) / 1000.
-    print "%.6f - %.6f - %.6f - %10d - %s" % (request_time, server_time, server_clock, length, route)
+  return [fetch_result(base_url, route) for route in queries]
+
+
+if __name__ == '__main__':
+  # seed website
+  base_url = sys.argv[1]
+  results = fetch_results(base_url)
+
+  for result in results:
+    print "%.6f - %.6f - %.6f - %10d - %s" % (
+      result['request_time'],
+      result['time'],
+      result['clock'],
+      result['length'],
+      result['route']
+    )
